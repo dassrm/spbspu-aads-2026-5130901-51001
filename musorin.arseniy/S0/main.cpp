6a61dd853eef21d66823e5399a9ca223b072f4d2
arseniymusorin@gmail.com <quaka@quaka.localdomain>
2026-03-05 21:28:12 +0300

    fix cg47

--- musorin.arseniy/S0/main.cpp
+++ musorin.arseniy/S0/main.cpp
@@ -1,5 +1,5 @@
-#include "credentials.hpp"
 #include <iostream>
+#include "credentials.hpp"
 int main()
 {
   musorin::out_credentials(std::cout) << "\n";
--- musorin.arseniy/S0/test-credentials.cpp
+++ musorin.arseniy/S0/test-credentials.cpp
@@ -1,6 +1,6 @@
-#include "credentials.hpp"
 #include <sstream>
 #include <boost/test/unit_test.hpp>
+#include "credentials.hpp"
 BOOST_AUTO_TEST_CASE(credentials_test)
 {
   std::ostringstream out;
