d952aa30d997726e6791d56ad2d4720f5c299774
dassrm <daryasm1rnoff@yandex.ru>
2026-03-14 15:00:10 +0300

    Add push/pop operations and element access with bounds checking

--- smirnova.daria/S1/list.hpp
+++ smirnova.daria/S1/list.hpp
@@ -3,6 +3,7 @@
 
 #include <cstddef>
 #include <stdexcept>
+#include <string>
 #include <type_traits>
 #include <utility>
 
@@ -169,6 +170,30 @@ public:
     return end();
   }
 
+  T& front()
+  {
+    checkNotEmpty("front");
+    return *begin();
+  }
+
+  const T& front() const
+  {
+    checkNotEmpty("front");
+    return *begin();
+  }
+
+  T& back()
+  {
+    checkNotEmpty("back");
+    return *iterator(sentinel_->prev_);
+  }
+
+  const T& back() const
+  {
+    checkNotEmpty("back");
+    return *const_iterator(sentinel_->prev_);
+  }
+
   bool empty() const
   {
     return size_ == 0;
@@ -177,6 +202,28 @@ public:
   std::size_t size() const
   {
     return size_;
+  }
+
+  void pushFront(T val)
+  {
+    insert(begin(), std::move(val));
+  }
+
+  void pushBack(T val)
+  {
+    insert(end(), std::move(val));
+  }
+
+  void popFront()
+  {
+    checkNotEmpty("popFront");
+    erase(begin());
+  }
+
+  void popBack()
+  {
+    checkNotEmpty("popBack");
+    erase(iterator(sentinel_->prev_));
   }
 
   iterator insert(iterator pos, T val)
@@ -216,6 +263,13 @@ public:
 private:
   detail::node_base_t* sentinel_;
   std::size_t size_;
+
+  void checkNotEmpty(const char* method) const
+  {
+    if (empty()) {
+      throw std::runtime_error(std::string(method) + "() on empty list");
+    }
+  }
 };
 
 }
