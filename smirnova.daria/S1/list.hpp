73b42af62b8f752292e8c56c6f2134c47d84df08
dassrm <daryasm1rnoff@yandex.ru>
2026-03-14 14:55:14 +0300

    Add iterator class with const support

--- smirnova.daria/S1/list.hpp
+++ smirnova.daria/S1/list.hpp
@@ -1,8 +1,9 @@
-// list.hpp
 #ifndef SMIRNOVA_LIST_HPP
 #define SMIRNOVA_LIST_HPP
 
 #include <cstddef>
+#include <stdexcept>
+#include <type_traits>
 #include <utility>
 
 namespace smirnova {
@@ -32,9 +33,101 @@ struct node_t: node_base_t {
 
 }
 
+template< class T, bool IsConst >
+class ListIter {
+public:
+  using ref = typename std::conditional< IsConst, const T&, T& >::type;
+  using ptr = typename std::conditional< IsConst, const T*, T* >::type;
+
+  ListIter():
+    node_(nullptr)
+  {}
+
+  template< bool C = IsConst, class = typename std::enable_if< C >::type >
+  ListIter(const ListIter< T, false >& other):
+    node_(other.node_)
+  {}
+
+  ref operator*() const
+  {
+    using node_ptr = typename std::conditional<
+      IsConst,
+      const detail::node_t< T >*,
+      detail::node_t< T >*
+    >::type;
+    return static_cast< node_ptr >(node_)->value_;
+  }
+
+  ptr operator->() const
+  {
+    return &(**this);
+  }
+
+  ListIter& operator++()
+  {
+    node_ = node_->next_;
+    return *this;
+  }
+
+  ListIter operator++(int)
+  {
+    ListIter tmp(*this);
+    ++(*this);
+    return tmp;
+  }
+
+  ListIter& operator--()
+  {
+    node_ = node_->prev_;
+    return *this;
+  }
+
+  ListIter operator--(int)
+  {
+    ListIter tmp(*this);
+    --(*this);
+    return tmp;
+  }
+
+  bool operator==(const ListIter& other) const
+  {
+    return node_ == other.node_;
+  }
+
+  bool operator!=(const ListIter& other) const
+  {
+    return !(*this == other);
+  }
+
+private:
+  friend class List< T >;
+  friend class ListIter< T, true >;
+
+  using base_ptr = typename std::conditional<
+    IsConst,
+    const detail::node_base_t*,
+    detail::node_base_t*
+  >::type;
+
+  base_ptr node_;
+
+  explicit ListIter(base_ptr node):
+    node_(node)
+  {}
+};
+
+template< class T >
+using LIter = ListIter< T, false >;
+
+template< class T >
+using LCIter = ListIter< T, true >;
+
 template< class T >
 class List {
 public:
+  using iterator = LIter< T >;
+  using const_iterator = LCIter< T >;
+
   List():
     sentinel_(new detail::node_base_t()),
     size_(0)
