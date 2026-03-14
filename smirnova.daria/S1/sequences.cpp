b1c0371404ac8034749f1df47dbfd902d6e8b9aa
dassrm <daryasm1rnoff@yandex.ru>
2026-03-14 15:04:10 +0300

    Implement transposition and sum calculation

--- smirnova.daria/S1/sequences.cpp
+++ smirnova.daria/S1/sequences.cpp
@@ -46,3 +46,53 @@ void smirnova::printNumbers(const smirnova::Numbers& nums)
   }
   std::cout << '\n';
 }
+
+smirnova::Transposed smirnova::buildTransposed(const smirnova::Sequences& seqs)
+{
+  List< LCIter< long long > > iters;
+  for (LCIter< NamedSeq > it = seqs.cbegin(); it != seqs.cend(); ++it) {
+    iters.pushBack(it->second.cbegin());
+  }
+
+  Transposed transposed;
+  while (true) {
+    Numbers row;
+    LCIter< NamedSeq > seqIt = seqs.cbegin();
+    LIter< LCIter< long long > > iterIt = iters.begin();
+    while (seqIt != seqs.cend()) {
+      if (*iterIt != seqIt->second.cend()) {
+        row.pushBack(**iterIt);
+        ++(*iterIt);
+      }
+      ++seqIt;
+      ++iterIt;
+    }
+    if (row.empty()) {
+      break;
+    }
+    transposed.pushBack(std::move(row));
+  }
+  return transposed;
+}
+
+long long smirnova::computeSum(const smirnova::Numbers& nums)
+{
+  long long sum = 0;
+  for (LCIter< long long > it = nums.cbegin(); it != nums.cend(); ++it) {
+    sum += *it;
+  }
+  return sum;
+}
+
+void smirnova::printSums(const smirnova::Transposed& transposed)
+{
+  bool first = true;
+  for (LCIter< Numbers > it = transposed.cbegin(); it != transposed.cend(); ++it) {
+    if (!first) {
+      std::cout << ' ';
+    }
+    std::cout << computeSum(*it);
+    first = false;
+  }
+  std::cout << '\n';
+}
